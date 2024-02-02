98d6a1951061ee3df4511b3fb5ec72fddcff1588
aman <ajaman190@gmail.com>
2024-02-02 16:13:01 +0530

    added routes
    Signed-off-by: aman <ajaman190@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Button from '@mui/material/Button';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import Dashboard from './Pages/Dashboard'
 
 function App() {
   return (
     <div className="App">
       <h1>it_works_on_local</h1>
       <Button variant="contained">Hello world</Button>
+      <Login/>
+      <BrowserRouter>
+        <Route path="/register" component={Register} />
+        <Route path="/dashboard" component={Dashboard} />
+      </BrowserRouter>
     </div>
   );
 }
