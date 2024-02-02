197750a8bd2b823a4646578e307f1c3a1c1bee2b
MUCCHU <harshwasnik160602@gmail.com>
2024-02-02 16:35:41 +0530

    added baseline css

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './Redux/store';
 import { Provider } from 'react-redux';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <CssBaseline enableColorScheme />
       <App />
     </Provider>
   </React.StrictMode>
