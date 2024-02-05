1c558c3a07dd344bcb0c59f4176f3d2f3bfbecab
MUCCHU <harshwasnik160602@gmail.com>
2024-02-05 10:29:50 +0530

    added jsconfig for better path management

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import Button from '@mui/material/Button';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Login from './Pages/Login'
-import Register from './Pages/Register'
-import Dashboard from './Pages/Dashboard'
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Dashboard from './Pages/Dashboard';
 
 function App() {
   return (
